48dbcd86dab36adcca55441761c305956d4be475
HelloAGit <95533237+HelloAGit@users.noreply.github.com>
2026-07-19 19:28:42 +0100

    Create App component with SignalList integration

---
+++ webapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignalList from "./components/SignalList";
+
+export default function App() {
+  return (
+    <div>
+      <h1>TxLINE Agent Lab Console</h1>
+      <SignalList />
+    </div>
+  );
+}
